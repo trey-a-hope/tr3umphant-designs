bb1942c85dcabe2e2e54b655dc3147255a24766c
Trey <trey.a.hope@gmail.com>
2019-09-16 12:01:17 -0400

    Finished pricing page for now.

--- web/src/app/pages/blog-page/blog-page.component.ts
+++ web/src/app/pages/blog-page/blog-page.component.ts
@@ -8,6 +8,8 @@ import { UrlService } from 'src/app/services/url.service';
 })
 export class BlogPageComponent implements OnInit {
 
+  public blogOneUrl = 'https://daytontechguide.com/test-this-new-app-for-daytons-artist-entrepreneur-community/?mc_cid=9351e5397c&mc_eid=cc9664f424&fbclid=IwAR019waR-XcIHFCtTWS3k5UBbgJOj3-tS1WjGWXw1_Gfh3pUn9gGnWdtSc8';
+
   constructor(private urlService: UrlService) { }
 
   ngOnInit() {
--- web/src/app/pages/pricing-page/pricing-page.component.ts
+++ web/src/app/pages/pricing-page/pricing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UrlService } from 'src/app/services/url.service';
 
 @Component({
   selector: 'pricing-page',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PricingPageComponent implements OnInit {
 
-  constructor() { }
+  public contractUrl = 'https://drive.google.com/open?id=1zJi05BfphQ08qi-u4AJBINNC6T-T7AKk';
+  public basicImageUrl = 'https://firebasestorage.googleapis.com/v0/b/tr3umphant-designs-bd77b.appspot.com/o/Images%2Fbasic_pricing.jpg?alt=media&token=f7508a59-4eaf-4353-b706-1e4beb11b3ae';
+  public standardImageUrl = 'https://firebasestorage.googleapis.com/v0/b/tr3umphant-designs-bd77b.appspot.com/o/Images%2Fstandard_pricing.jpg?alt=media&token=8c2bd444-7b99-4d52-bb72-325912116d03';
+  public premiumImageUrl = 'https://firebasestorage.googleapis.com/v0/b/tr3umphant-designs-bd77b.appspot.com/o/Images%2Fpremium_pricing.jpg?alt=media&token=84413dd2-dbf2-4e2f-8ae0-9b3eb9511dcf';
+
+  constructor(public urlService: UrlService) { }
 
   ngOnInit() {
   }
