8498ff03fdeaec94f1683cdd3919e38e5984a019
Trey Hope <treyhope@Treys-Air.lan1>
2019-10-20 02:11:12 -0400

    Updated pricing.

--- web/src/app/pages/pricing-page/pricing-page.component.ts
+++ web/src/app/pages/pricing-page/pricing-page.component.ts
@@ -11,12 +11,12 @@ export class PricingPageComponent implements OnInit {
   public basicImageUrl = 'https://firebasestorage.googleapis.com/v0/b/tr3umphant-designs-bd77b.appspot.com/o/Images%2Fbasic_pricing.jpg?alt=media&token=f7508a59-4eaf-4353-b706-1e4beb11b3ae';
   public standardImageUrl = 'https://firebasestorage.googleapis.com/v0/b/tr3umphant-designs-bd77b.appspot.com/o/Images%2Fstandard_pricing.jpg?alt=media&token=8c2bd444-7b99-4d52-bb72-325912116d03';
   public premiumImageUrl = 'https://firebasestorage.googleapis.com/v0/b/tr3umphant-designs-bd77b.appspot.com/o/Images%2Fpremium_pricing.jpg?alt=media&token=84413dd2-dbf2-4e2f-8ae0-9b3eb9511dcf';
-  public basicPrice = 1000;
-  public basicDeposit = Math.ceil((this.basicPrice * 0.33) / 10) * 10;
-  public standardPrice = 2000;
-  public standardDeposit = Math.ceil((this.standardPrice * 0.33) / 10) * 10;
-  public premiumPrice = 3000;
-  public premiumDeposit = Math.ceil((this.premiumPrice * 0.33) / 10) * 10;
+  public basicPrice = 2000;
+  public basicDeposit = this.basicPrice * 0.5;
+  public standardPrice = 4000;
+  public standardDeposit = this.standardPrice * 0.5;
+  public premiumPrice = 5000;
+  public premiumDeposit = this.premiumPrice * 0.5;
 
   constructor(public urlService: UrlService) { }
 
