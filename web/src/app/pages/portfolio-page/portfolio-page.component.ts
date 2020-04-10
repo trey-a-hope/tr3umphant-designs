fbc9cde772f7d2eb3910745945c4c85bf674ff89
Trey Hope <trey.a.hope@gmail.com>
2020-04-10 07:07:41 -0400

    Fixed YouTube embedded links on blog page.

--- web/src/app/pages/portfolio-page/portfolio-page.component.ts
+++ web/src/app/pages/portfolio-page/portfolio-page.component.ts
@@ -7,24 +7,24 @@ import { UrlService } from 'src/app/services/url.service';
   styleUrls: ['./portfolio-page.component.css']
 })
 export class PortfolioPageComponent implements OnInit {
-  public hiddenGems = {
-    'ios': 'https://apps.apple.com/us/app/hxddxn-gxms/id1477749493',
-    'android': 'https://play.google.com/store/apps/details?id=com.io.hiddengems_flutter&hl=en_US'
-  };
+  // public hiddenGems = {
+  //   'ios': 'https://apps.apple.com/us/app/hxddxn-gxms/id1477749493',
+  //   'android': 'https://play.google.com/store/apps/details?id=com.io.hiddengems_flutter&hl=en_US'
+  // };
 
-  public tr3Designs = {
-    'ios': 'https://apps.apple.com/us/app/tr3designs/id1390134234',
-    'android': 'https://play.google.com/store/apps/details?id=com.io.tr3designs_flutter&hl=en_US'
-  }
+  // public tr3Designs = {
+  //   'ios': 'https://apps.apple.com/us/app/tr3designs/id1390134234',
+  //   'android': 'https://play.google.com/store/apps/details?id=com.io.tr3designs_flutter&hl=en_US'
+  // }
 
-  public unreadReader = {
-    'android': ''
-  }
+  // public unreadReader = {
+  //   'android': ''
+  // }
 
-  public litpic = {
-    'ios': 'https://apps.apple.com/us/app/lit-pic-lithophane-creator/id1489604752',
-    'android': 'https://play.google.com/store/apps/details?id=com.io.litpic'
-  }
+  // public litpic = {
+  //   'ios': 'https://apps.apple.com/us/app/lit-pic-lithophane-creator/id1489604752',
+  //   'android': 'https://play.google.com/store/apps/details?id=com.io.litpic'
+  // }
 
   constructor(public urlService: UrlService) { }
 
